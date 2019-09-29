ed1a539d7abf346da140db64d44e199af9b37c41
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2019-09-29 16:49:13 +0200

    First working prototype.

--- scenes/House.gd
+++ scenes/House.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+class_name House
+
 const MEAN_RECT_WIDTH = 100
 const RECT_WIDTH_VARIATION = 30
 
@@ -8,6 +10,9 @@ const RECT_HEIGHT_VARIATION = 50
 
 var rnd = RandomNumberGenerator.new()
 
+var Enemy = preload("res://scenes/Enemy.tscn")
+var enemy
+
 func _ready():
 	rnd.randomize()
 	
@@ -15,9 +20,30 @@ func _ready():
 	$Building.rect_size.x = MEAN_RECT_WIDTH + rnd.randi_range(-RECT_WIDTH_VARIATION, RECT_WIDTH_VARIATION)
 	$Building.rect_size.y = MEAN_RECT_HEIGHT + rnd.randi_range(-RECT_HEIGHT_VARIATION, RECT_HEIGHT_VARIATION)
 	$Building.rect_position.y = get_viewport_rect().size.y - $Building.rect_size.y
+	
+	$Building/VisibilityNotifier2D.rect.size.x = $Building.rect_size.x
+	$Building/VisibilityNotifier2D.rect.size.y = $Building.rect_size.y
+	
+	# Spawn an enemy atop the house
+	enemy = Enemy.instance()
+	enemy.rect_position.y = $Building.rect_position.y - 40
+	add_child(enemy)
 
 func set_building_x(new_x):
 	$Building.rect_position.x = new_x
+	enemy.rect_position.x = new_x + 10
+
+func get_building_x() -> int:
+	return $Building.rect_position.x
+
+func move_building(delta_x):
+	$Building.rect_position.x -= delta_x
+	enemy.rect_position.x -= delta_x
 
 func get_building_width():
 	return $Building.rect_size.x
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	print("Removing building")
+	queue_free()
