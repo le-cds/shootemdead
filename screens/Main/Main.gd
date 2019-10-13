71800d0bab2e5cd44af765d14fef77bca3a34d5c
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2019-10-13 18:18:23 +0200

    Moved and extended the main scene.

---
+++ screens/Main/Main.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+
+####################################################################################
+# State
+
+# The base speed for all of our EndlessSidescroller instances.
+var _base_speed := 20 setget set_base_speed, get_base_speed
+
+
+####################################################################################
+# Scene Objects
+
+onready var state_machine := $StateMachine
+onready var state_menu_main := $StateMachine/MenuMain
+
+
+####################################################################################
+# Lifecycle
+
+func _ready() -> void:
+	state_machine.transition_push(state_menu_main)
+
+
+####################################################################################
+# Getters / Setters
+
+func get_base_speed() -> int:
+	return _base_speed
+
+# Sets the base speed to a new value and updates all EndlessSidescrollers in the
+# "sidescrollers" group.
+func set_base_speed(new_base_speed: int) -> void:
+	if new_base_speed > 0:
+		_base_speed = new_base_speed
+		
+		# Update our sidescrollers
+		for scroller in get_tree().get_nodes_in_group("sidescrollers"):
+			(scroller as EndlessSidescroller).base_speed = _base_speed
