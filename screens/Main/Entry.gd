5a2e94964b271996e2fed8b7f01edee3d70d37d3
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2019-11-15 11:37:20 +0100

    Splash screen scene now properly loads the main scene in the background. #1

---
+++ screens/Main/Entry.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+# Shows the splash screen while loading the main scene. Finally transitions
+# smoothly to the main menu.
+
+####################################################################################
+# Scene Objects and State
+
+onready var _splash: Node2D = $Splash
+onready var _slideshow: Slideshow = $Splash/Slideshow
+onready var _animation: AnimationPlayer = $Splash/AnimationPlayer
+onready var _main_parent: Node2D = $MainParent
+
+var _main_scene_loader: ResourceInteractiveLoader
+
+
+####################################################################################
+# Scene Lifecycle
+
+func _ready() -> void:
+	_slideshow.start()
+	
+	# Start loading the main scene
+	_main_scene_loader = ResourceLoader.load_interactive("res://screens/Main/Main.tscn")
+
+
+func _process(delta: float) -> void:
+	# Once our main scene has finished loading, add it to the scene tree so it's
+	# ready when the splash screen disappears.
+	if _main_scene_loader != null and _main_scene_loader.poll() == ERR_FILE_EOF:
+		_main_parent.add_child(_main_scene_loader.get_resource().instance())
+		
+		# Remove loader and disable processing for this node -- we're done!
+		_main_scene_loader = null
+		set_process(false)
+
+
+####################################################################################
+# Event Handling
+
+func _on_Slideshow_slideshow_finished():
+	# Fade out our splash screen and remove it
+	_animation.play("FadeSplash")
+	yield(_animation, "animation_finished")
+	_splash.queue_free()
--- screens/Main/Splash.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-onready var _slideshow: Slideshow = $Slideshow
-
-func _ready():
-	_slideshow.start()
