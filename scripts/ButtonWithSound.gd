edbbd52ba09ca45e599eb46efb81aa701979974c
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2019-12-13 20:54:46 +0100

    UI sounds. #16

---
+++ scripts/ButtonWithSound.gd
@@ -0,0 +1,28 @@
+extends Button
+
+# This is a button which is happy to play sounds when the mouse cursor enters it
+# or clicks on it. This button is hard-coded for our sounds through defaults.
+class_name ButtonWithSound
+
+
+# The sound we play when the mouse cursor enters us.
+export(AudioStream) var hover_sound: AudioStream = preload("res://assets/Sounds/shot_big_1.ogg")
+# The sound we play when the mouse cursor clicks us.
+export(AudioStream) var click_sound: AudioStream = preload("res://assets/Sounds/shot_big_2.ogg")
+
+
+func _init() -> void:
+	# We'll listen for button presses
+	connect("button_down", self, "_play_click_sound")
+
+
+func _notification(what) -> void:
+	match what:
+		NOTIFICATION_MOUSE_ENTER:
+			if hover_sound:
+				SoundPlayer.play_sound(self, hover_sound)
+
+
+func _play_click_sound() -> void:
+	if click_sound:
+		SoundPlayer.play_sound(self, click_sound)
